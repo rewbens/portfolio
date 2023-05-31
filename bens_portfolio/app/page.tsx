4a2bf7edf93687b0629ce608c5a9d213895cc790
Ben Rew <rewbens@googlemail.com>
2023-05-31 14:33:07 +0100

    tailwind recalibrated

--- bens_portfolio/app/page.tsx
+++ bens_portfolio/app/page.tsx
@@ -3,7 +3,8 @@ import { getProjects } from "../sanity / schemas/sanity-utils";
 export default async function Home() {
   const projects = await getProjects();
   return (
-    <div>
+    <div className="max-w-5xl mx-auto py-20">
+      <h1 className="text-7xl font-extrabold">Hello I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Benjamin!</span></h1>
       {projects.map((project) => (
         <div key={project._id}>{project.name}</div>
       ))}
